c069f0e75e33119b2c51e3b66ce61841fe899855
Bru Cardoso <123430397+brucardosocoder@users.noreply.github.com>
2024-01-26 18:51:27 -0300

    Subi os arquivos do site Orquestra Pau e Corda

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Código para o scroll automático
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", smoothScroll);
+    });
+
+    function smoothScroll(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector("header").offsetHeight,
+            behavior: "smooth"
+        });
+    }
+});
